ff8f0f38aa120fccc55134aed556c2aefe937903
rrubyist <628326+rrubyist@users.noreply.github.com>
2022-01-25 00:49:39 +0100

    WIP: Simplification of the logic.

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -13,15 +13,27 @@ defmodule GildedRose do
   end
 
   defp increase_quality(%Item{} = item) do
-    %{item | quality: item.quality + 1}
+    if item.quality < 50 do
+      %{item | quality: item.quality + 1}
+    else
+      item
+    end
   end
 
   defp decrease_quality(%Item{} = item) do
-    decrease_attribute(item, :quality)
+    if item.quality > 0 do
+      decrease_attribute(item, :quality)
+    else
+      item
+    end
   end
 
   defp decrease_attribute(%Item{} = item, attribute) do
-    %{item | attribute => Map.get(item, attribute) - 1}
+    if item.name == "Sulfuras, Hand of Ragnaros" do
+      item
+    else
+      %{item | attribute => Map.get(item, attribute) - 1}
+    end
   end
 
   @spec update_item(atom | %{:name => String.t(), :quality => integer(), optional(any) => any}) ::
@@ -29,40 +41,26 @@ defmodule GildedRose do
   def update_item(item) do
     item = cond do
       item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-        if item.name != "Sulfuras, Hand of Ragnaros" && item.quality > 0 do
-          decrease_quality(item)
-        else
-          item
-        end
+        decrease_quality(item)
       true ->
+        item = increase_quality(item)
         cond do
-          item.quality < 50 ->
-            item = increase_quality(item)
+          item.name == "Backstage passes to a TAFKAL80ETC concert" ->
             cond do
-              item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-                cond do
-                  item.sell_in < 11 ->
-                    cond do
-                      item.quality < 50 ->
-                        increase_quality(item)
-                      true -> item
-                    end
-                  true -> item
-                end
+              item.sell_in < 11 ->
+                increase_quality(item)
               true -> item
             end
           true -> item
         end
     end
-    item = cond do
-      item.name != "Sulfuras, Hand of Ragnaros" ->
-        item |> decrease_attribute(:sell_in)
-      true -> item
-    end
+    item = item |> decrease_attribute(:sell_in)
     cond do
       item.sell_in < 0 ->
-        cond do
-          item.name != "Aged Brie" ->
+        case item.name do
+          "Aged Brie" ->
+            increase_quality(item)
+          _ ->
             cond do
               item.name != "Backstage passes to a TAFKAL80ETC concert" ->
                 cond do
@@ -74,13 +72,8 @@ defmodule GildedRose do
                     end
                   true -> item
                 end
-              true -> %{item | quality: item.quality - item.quality}
-            end
-          true ->
-            cond do
-              item.quality < 50 ->
-                increase_quality(item)
-              true -> item
+              true ->
+                %{item | quality: item.quality - item.quality}
             end
         end
       true -> item
--- elixir/test/gilded_rose_test.exs
+++ elixir/test/gilded_rose_test.exs
@@ -2,88 +2,102 @@ defmodule GildedRoseTest do
   use ExUnit.Case
   doctest(GildedRose)
 
-  # test "update_quality" do
-  #   items = GildedRose.update_quality(
-  #     [%Item{name: "foo", sell_in: 0, quality: 0}]
-  #   )
-  #   %{name: name} = List.first(items)
-
-  #   assert "foo" == name
-  # end
-
-  describe "when it has well recognized names" do
-    test "Aged Brie: sell_in 0, quality 0" do
-      items = [%Item{name: "Aged Brie", sell_in: 0, quality: 0}]
+  describe "Aged Brie" do
+    test "sell_in: 0, quality: 0", context do
+      items = [%Item{name: context.describe, sell_in: 0, quality: 0}]
       [item] = GildedRose.update_quality(items)
 
       assert item.quality == 2
       assert item.sell_in == -1
     end
 
-    test "Aged Brie: sell_in 3, quality 3" do
-      items = [%Item{name: "Aged Brie", sell_in: 3, quality: 3}]
+    test "sell_in: 3, quality: 3", context do
+      items = [%Item{name: context.describe, sell_in: 3, quality: 3}]
       [item] = GildedRose.update_quality(items)
 
       assert item.quality == 4
       assert item.sell_in == 2
     end
+  end
 
-    test "the sell_in and the quality stays on its position" do
-      items = [%Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 0}]
+  # "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+  describe "Sulfuras, Hand of Ragnaros" do
+    test "sell_in: 3, quality: 3", context do
+      items = [%Item{name: context.describe, sell_in: 3, quality: 3}]
       [item] = GildedRose.update_quality(items)
 
-      assert item.quality == 0
-      assert item.sell_in == 0
-    end
-
-    test "sell_in < 11 and quality < 50" do
-      items = [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 49}]
-      [item] = GildedRose.update_quality(items)
-
-      assert item.quality == 50
-      assert item.sell_in == 9
-    end
-
-    test "sell_in < 6 and quality < 50" do
-      items = [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 49}]
-      [item] = GildedRose.update_quality(items)
-
-      assert item.quality == 50
-      assert item.sell_in == 4
-    end
-
-    test "sell_in < 11 and quality > 50" do
-      items = [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 51}]
-      [item] = GildedRose.update_quality(items)
-
-      assert item.quality == 51
-      assert item.sell_in == 9
+      assert item.quality == 3
+      assert item.sell_in == 3
     end
   end
 
-  describe "when it has unrecognized name" do
-    # test "drops the price 123" do
-    #   items = [%Item{name: "Unrecognized name", sell_in: 0, quality: 49}]
-    #   [item] = GildedRose.update_quality(items)
-
-    #   assert item.quality == 0
-    #   assert item.sell_in == -1
-    # end
-
-    test "drops the price" do
-      items = [%Item{name: "Unrecognized name", sell_in: 0, quality: 0}]
+  describe "Backstage passes to a TAFKAL80ETC concert" do
+    test "sell_in: 10, quality: 49", context do
+      items = [%Item{name: context.describe, sell_in: 10, quality: 49}]
       [item] = GildedRose.update_quality(items)
 
-      assert item.quality == 0
-      assert item.sell_in == -1
+      assert item.sell_in == 9
+      assert item.quality == 50
     end
 
-    test "loses the sell_in and the quality points" do
-      items = [%Item{name: "Unrecognized name", sell_in: 10, quality: 1}]
+    test "sell_in: 5, quality: 49", context do
+      items = [%Item{name: context.describe, sell_in: 5, quality: 49}]
       [item] = GildedRose.update_quality(items)
 
-      assert item.quality == 0
+      assert item.sell_in == 4
+      assert item.quality == 50
+    end
+
+    test "sell_in: 10, quality: 51", context do
+      items = [%Item{name: context.describe, sell_in: 10, quality: 50}]
+      [item] = GildedRose.update_quality(items)
+
       assert item.sell_in == 9
+      assert item.quality == 50
+    end
+
+    test "sell_in -5, quality: 5", context do
+      items = [%Item{name: context.describe, sell_in: -5, quality: 5}]
+      [item] = GildedRose.update_quality(items)
+
+      assert item.sell_in == -6
+      assert item.quality == 0
+    end
+  end
+
+  describe "unrecognized name" do
+    # Once the sell by date has passed, Quality degrades twice as fast
+    test "sell_in: -1, quality: 5", context do
+      items = [%Item{name: context.describe, sell_in: -1, quality: 3}]
+      [item] = GildedRose.update_quality(items)
+
+      assert item.sell_in == -2
+      assert item.quality == 1
+    end
+
+    # The Quality of an item is never negative
+    test "sell_in: -5, quality: 1", context do
+      items = [%Item{name: context.describe, sell_in: -1, quality: 1}]
+      [item] = GildedRose.update_quality(items)
+
+      assert item.sell_in == -2
+      assert item.quality == 0
+    end
+
+    test "sell_in: 0, quality: 0", context do
+      items = [%Item{name: context.describe, sell_in: 0, quality: 0}]
+      [item] = GildedRose.update_quality(items)
+
+      assert item.sell_in == -1
+      assert item.quality == 0
+    end
+
+    test "sell_in: 10, quality: 1", context do
+      items = [%Item{name: context.describe, sell_in: 10, quality: 1}]
+      [item] = GildedRose.update_quality(items)
+
+      assert item.sell_in == 9
+      assert item.quality == 0
     end
   end
 
